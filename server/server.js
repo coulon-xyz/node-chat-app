545550c32b0dec3100cadcfcb924624158ff3c1d
Coulon <julien.j.coulon@gmail.com>
2016-11-29 16:59:56 +0100

    Connect and disconnet handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
-const express = require('express');
 const path = require('path'); // Built in module, doesn't need npm
+const http = require('http'); // idem
+const express = require('express');
+const socketIO = require('socket.io');
 
 // Variables declaration
 const publicPath = path.join(__dirname,'../public')
-var app = express();
 const port = process.env.PORT || 3000
+var app = express();
+var server = http.createServer(app); // using http server and not an express
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port , () => {
+// on register an event listener
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  });
+
+});
+
+
+
+server.listen(port , () => {
   console.log(`Server started on port ${port}`)
 });
