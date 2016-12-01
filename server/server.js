f34428a91d55d714a53a64bd66d5096e41dca045
Coulon <julien.j.coulon@gmail.com>
2016-12-01 10:23:20 +0100

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log("new message:", message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is an ack from the server');
+    callback(); // acknowledging
   });
 
   socket.on('createLocationMessage', (coords) => {
