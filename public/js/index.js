521b8a2301d2c3116375c7e0cc47cc1def09a9e6
Coulon <julien.j.coulon@gmail.com>
2016-12-06 16:01:17 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom ()  {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child'); // selecting last msg
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('connected to server');
 });
@@ -18,7 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,13 +48,7 @@ socket.on('newLocationMessage', function (message) {
    });
 
    jQuery('#messages').append(html);
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
-  //
-  // li.text(`${message.from} @ ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+   scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
